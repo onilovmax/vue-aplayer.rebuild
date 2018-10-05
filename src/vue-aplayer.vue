136e47e2ae11a8558e585eea68eb59f6d5a393c1
Max <maxonilov@gmail.com>
2018-10-05 12:42:12 +0300

    7

--- src/vue-aplayer.vue
+++ src/vue-aplayer.vue
@@ -11,7 +11,7 @@
     :style="floatStyleObj"
   >
 
-    <button class="btn btn-danger" style="padding: 0" v-if="!showPlayer && !mini"  @click="displayPlayer(true)">Открыть</button>
+    <button class="btn btn-danger" style="padding: 0; margin: 10px;" v-if="!showPlayer && !mini"  @click="displayPlayer(true)">Открыть плеер</button>
 
     <button class="btn btn-danger" style="padding: 0" v-if="showPlayer && mini" @click="displayPlayer(false)">Закрыть</button>
     <div class="aplayer-body" v-if="showPlayer && mini">
